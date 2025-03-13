4c554962ebe71629a90351fa394f402810d21204
tg <eat.some.trouble@gmail.com>
2025-03-13 14:26:01 +0300

    created user model

---
+++ src/main/java/itmo/tg/airbnb_business/model/Role.java
@@ -0,0 +1,8 @@
+package itmo.tg.airbnb_business.model;
+
+public enum Role {
+
+    ROLE_USER,
+    ROLE_ADMIN
+
+}
---
+++ src/main/java/itmo/tg/airbnb_business/model/User.java
@@ -0,0 +1,45 @@
+package itmo.tg.airbnb_business.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    @NotNull
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    @NotNull
+    private String password;
+
+    @Column(name = "role", nullable = false)
+    @NotNull
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+}
