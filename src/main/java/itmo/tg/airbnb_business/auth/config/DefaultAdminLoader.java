13d0118b8576eb68e601275379c7b1d37eabd7ea
tg <eat.some.trouble@gmail.com>
2025-03-13 14:47:01 +0300

    created default admin loader

---
+++ src/main/java/itmo/tg/airbnb_business/auth/config/DefaultAdminLoader.java
@@ -0,0 +1,38 @@
+package itmo.tg.airbnb_business.auth.config;
+
+import itmo.tg.airbnb_business.auth.model.Role;
+import itmo.tg.airbnb_business.auth.model.User;
+import itmo.tg.airbnb_business.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DefaultAdminLoader {
+
+    @Value("${credentials.admin.username}")
+    private String adminUsername;
+
+    @Value("${credentials.admin.password}")
+    private String adminPassword;
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadDefaultAdmin() {
+        if (!userRepository.existsByUsername(adminUsername)) {
+            User admin = User.builder()
+                    .username(adminUsername)
+                    .password(passwordEncoder.encode(adminPassword))
+                    .role(Role.ROLE_ADMIN)
+                    .build();
+            userRepository.save(admin);
+        }
+    }
+
+}
