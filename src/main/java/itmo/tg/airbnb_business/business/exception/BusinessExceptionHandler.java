7d8bc834b4a1c401000fccfe2520931242877e9e
tg <eat.some.trouble@gmail.com>
2025-03-25 16:20:57 +0300

    nice exceptions

--- src/main/java/itmo/tg/airbnb_business/business/exception/BusinessExceptionHandler.java
+++ src/main/java/itmo/tg/airbnb_business/business/exception/BusinessExceptionHandler.java
@@ -3,6 +3,8 @@ package itmo.tg.airbnb_business.business.exception;
 import itmo.tg.airbnb_business.business.exception.exceptions.NotAllowedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -22,6 +24,39 @@ public class BusinessExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+
+        var fieldErrors = ex.getFieldErrors();
+        var fieldNames = fieldErrors.stream().map(FieldError::getField).distinct().toList();
+
+        var sb = new StringBuilder();
+
+        for (var field : fieldNames) {
+
+            var messages = fieldErrors.stream()
+                    .filter(e -> e.getField().equals(field))
+                    .map(FieldError::getDefaultMessage).toList();
+
+            sb.append(field).append(' ');
+
+            var iter = messages.iterator();
+            while (iter.hasNext()) {
+                sb.append(iter.next());
+                if (iter.hasNext()) {
+                    sb.append(", ");
+                }
+            }
+
+            sb.append('\n');
+
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
+
     }
 
 }
--- src/main/java/itmo/tg/airbnb_business/business/exception/exceptions/NegativeCostException.java
+++
@@ -1,5 +0,0 @@
-package itmo.tg.airbnb_business.business.exception.exceptions;
-
-public class NegativeCostException extends RuntimeException {
-    public NegativeCostException(String message) {}
-}
