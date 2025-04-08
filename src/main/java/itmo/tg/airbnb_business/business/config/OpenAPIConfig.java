019124661b283d32a862d6065a81c9749395e76b
tg <eat.some.trouble@gmail.com>
2025-04-08 14:03:22 +0300

    a little more logs

--- src/main/java/itmo/tg/airbnb_business/business/config/OpenAPIConfig.java
+++ src/main/java/itmo/tg/airbnb_business/business/config/OpenAPIConfig.java
@@ -5,14 +5,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class OpenAPIConfig {
+
+    @Value("${server.port}")
+    private int port;
 
     @Bean
     public OpenAPI customOpenAPI() {
+        log.info("Swagger UI available at http://localhost:{}/swagger-ui/index.html", port);
         return new OpenAPI()
                 .info(new Info()
                         .title("Airbnb Fines & Penalties API")
--- src/main/java/itmo/tg/airbnb_business/security/config/DefaultAdminLoader.java
+++ src/main/java/itmo/tg/airbnb_business/security/config/DefaultAdminLoader.java
@@ -4,6 +4,7 @@ import itmo.tg.airbnb_business.security.model.Role;
 import itmo.tg.airbnb_business.security.model.User;
 import itmo.tg.airbnb_business.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DefaultAdminLoader {
 
     @Value("${credentials.admin.username}")
@@ -32,6 +34,7 @@ public class DefaultAdminLoader {
                     .role(Role.ROLE_ADMIN)
                     .build();
             userRepository.save(admin);
+            log.info("Created default admin");
         }
     }
 
